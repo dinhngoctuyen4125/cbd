eb676201f3ba7d3887aeb9c2b372025891d64e45
dinhngoctuyen4125 <tuyenotk4125@gmail.com>
2026-09-19 00:58:37 +0700

    lmao

--- run_script.sh
+++ run_script.sh
@@ -62,7 +62,7 @@ python scripts/extract_cbd_dfb_basis.py \
     --max_len ${MAX_LEN} \
     --top_k ${TOP_K} \
     --seed ${SEED} \
-    --batch_size 16 \
+    --batch_size 8 \
     --output_dir "${BASIS_DIR}"
 
 echo ""
