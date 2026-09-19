b27e7aec18d103e21e126881c38a3f6bbc8d1d5f
dinhngoctuyen4125 <tuyenotk4125@gmail.com>
2026-09-19 07:54:30 +0700

    lmao

--- run_script.sh
+++ run_script.sh
@@ -84,6 +84,8 @@ python scripts/hf_forget_train.py \
     cbd_dfb_basis_path="${BASIS_FILE}" \
     seed=${SEED} \
     lora_seed=${SEED} \
+    trainer.batch_size=8 \
+    trainer.gradient_accumulation_steps=1 \
     OUTPUTMODELDIR="${TRAIN_OUTPUT_DIR}"
 
 # Find the latest checkpoint (may be nested in subdirectories)
